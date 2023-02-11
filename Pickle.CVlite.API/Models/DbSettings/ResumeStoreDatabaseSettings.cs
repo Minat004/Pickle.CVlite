59f27203f65a897185e07725bd0120482e7e9630
Minato <umi0n3@gmail.com>
2023-02-11 13:56:23 +0100

    add db service and settings

---
+++ Pickle.CVlite.API/Models/DbSettings/ResumeStoreDatabaseSettings.cs
@@ -0,0 +1,10 @@
+namespace Pickle.CVlite.API.Models.DbSettings;
+
+public class ResumeStoreDatabaseSettings
+{
+    public string? ConnectionString { get; set; }
+
+    public string? DatabaseName { get; set; }
+
+    public string? CollectionName { get; set; }
+}
---
+++ Pickle.CVlite.API/Services/ResumesService.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Pickle.CVlite.API.Models;
+using Pickle.CVlite.API.Models.DbSettings;
+
+namespace Pickle.CVlite.API.Services;
+
+public class ResumesService
+{
+    private readonly IMongoCollection<Resume> _collection;
+
+    public ResumesService(IOptions<ResumeStoreDatabaseSettings> resumeStoreDbSettings)
+    {
+        var mongoClient = new MongoClient(resumeStoreDbSettings.Value.ConnectionString);
+
+        var mongoDataBase = mongoClient.GetDatabase(resumeStoreDbSettings.Value.DatabaseName);
+
+        _collection = mongoDataBase.GetCollection<Resume>(resumeStoreDbSettings.Value.CollectionName);
+    }
+    
+    public async Task<List<Resume>> GetAsync() =>
+        await _collection.Find(_ => true).ToListAsync();
+
+    public async Task<Resume?> GetAsync(string id) =>
+        await _collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(Resume newResume) =>
+        await _collection.InsertOneAsync(newResume);
+
+    public async Task UpdateAsync(string id, Resume updatedResume) =>
+        await _collection.ReplaceOneAsync(x => x.Id == id, updatedResume);
+
+    public async Task RemoveAsync(string id) =>
+        await _collection.DeleteOneAsync(x => x.Id == id);
+}

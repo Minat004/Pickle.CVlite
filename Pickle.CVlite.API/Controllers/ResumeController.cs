1c1d2ca7a4cd4ef08ab4a62dcf4cdab8f04ea5b0
Minato <umi0n3@gmail.com>
2023-02-11 13:58:43 +0100

    resume controller methods

--- Pickle.CVlite.API/Controllers/ResumeController.cs
+++ Pickle.CVlite.API/Controllers/ResumeController.cs
@@ -1,8 +1,59 @@
 using Microsoft.AspNetCore.Mvc;
+using Pickle.CVlite.API.Models;
+using Pickle.CVlite.API.Services;
 
 namespace Pickle.CVlite.API.Controllers;
 
+[ApiController]
+[Route("api/[controller]")]
 public class ResumeController : ControllerBase
 {
+    private readonly ResumesService _resumesService;
     
+    public ResumeController(ResumesService resumesService)
+    {
+        _resumesService = resumesService;
+    }
+
+    [HttpGet]
+    public async Task<List<Resume>> Get() => await _resumesService.GetAsync();
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Resume resume)
+    {
+        await _resumesService.CreateAsync(resume);
+        return CreatedAtAction(nameof(Get), new { id = resume.Id }, resume);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Resume updatedResume)
+    {
+        var resume = await _resumesService.GetAsync(id);
+
+        if (resume is null)
+        {
+            return NotFound();
+        }
+
+        updatedResume.Id = resume.Id;
+
+        await _resumesService.UpdateAsync(id, updatedResume);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var resume = await _resumesService.GetAsync(id);
+
+        if (resume is null)
+        {
+            return NotFound();
+        }
+
+        await _resumesService.RemoveAsync(id);
+
+        return NoContent();
+    }
 }
